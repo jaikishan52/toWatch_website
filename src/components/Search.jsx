f4cf1cdf93e03c24b4dc1c93acf7e1d7f427317d
Jai Kishan <jaikishan.timmapatruni@vitg.us>
2025-05-20 00:03:11 -0700

    just added a search bar component and structure of the project

---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <img src="./public/search.svg" alt="search" />
+      <input
+      type = "text"
+      placeholder="Search through thousands of movies"
+      value = {searchTerm}
+      onChange = {(event)=>setSearchTerm(event.target.value)}/>
+    </div>
+  )
+} //we should never mutate props or state
+
+// const Search = (props) => {
+//   return <div className="text-white text-3xl">{props.searchTerm}</div>
+// }
+
+export default Search
