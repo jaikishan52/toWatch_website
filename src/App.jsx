c0e326a770831474ed84a078da3cf4eb2daec803
Jai Kishan <jaikishan.timmapatruni@vitg.us>
2025-05-24 22:53:42 -0700

    Added Spinner component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useEffect } from "react"
 import { useState } from "react"
 import Search from "./components/Search"
+import Spinner from "./components/Spinner"
 
 const API_BASE_URL = "https://api.themoviedb.org/3"
 
@@ -14,11 +15,45 @@ const API_OPTIONS = {
     Authorization: `Bearer ${API_KEY}`,
   },
 }
-
+// text-red-500 sets the red color shade for the text
+//
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("")
 
-  useEffect(() => {}, [])
+  const [errorMessage, setErrorMessage] = useState("")
+
+  const [movieList, setMovieList] = useState([])
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const fetchMovies = async () => {
+    setIsLoading(true)
+    setErrorMessage("")
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const response = await fetch(endpoint, API_OPTIONS) //make https requests , get the data from apis
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies")
+      }
+
+      const data = await response.json()
+      //console.log(data)
+      if (data.response === "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies")
+        setMovieList([])
+      }
+      setMovieList(data.results || [])
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`)
+      setErrorMessage(`Error fetching movies: Please try again later`)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  }, [])
 
   return (
     <div>
@@ -29,9 +64,26 @@ const App = () => {
           <h1>
             Find <span className="text-gradient">Movies </span>You'll Enjoy Without the Hassle
           </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <h1 className="text-white">{searchTerm}</h1>
+        <section className="all-movies">
+          <h2 className="mt-[40px]">All Movies</h2>
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {/* when we are trying to delete about particular movie or something we 
+              need to point it out to the particular id so good to maintain a key */}
+              {movieList.map((movie) => (
+                <p key={movie.id} className="text-white">
+                  {movie.title}{" "}
+                </p>
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
     </div>
   )
